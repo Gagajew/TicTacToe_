69719ad829a1be5e0222922f70b85cc3cf7e9ad9
Gagajew <wi23b164@technikum-wien.at>
2024-06-28 15:11:52 +0200

    update Board Klasse

--- TicTacToe2/src/main/java/org/example/Board.java
+++ TicTacToe2/src/main/java/org/example/Board.java
@@ -1,31 +1,31 @@
 package org.example;
 
 public class Board {
-    private final char[][] cells;
+            private final char[][] cells;
 
 
-    public Board() {
-        this.cells = new char[3][3];
-    }
+            public Board() {
+                this.cells = new char[3][3];
+            }
 
-    public boolean isCellEmpty(int x, int y) {
-        return this.cells[x][y] == ' ';
-    }
+            public boolean isCellEmpty(int x, int y) {
+                return this.cells[x][y] == ' ';
+            }
 
-    public void place(int x, int y, char marker) {
-        if (isCellEmpty(x, y)) {
-            this.cells[x][y] = marker;
-        }
-    }
-
-    public boolean isFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (cells[i][j] == ' ') {
-                    return false;
+            public void place(int x, int y, char marker) {
+                if (isCellEmpty(x, y)) {
+                    this.cells[x][y] = marker;
                 }
             }
-        }
+
+            public boolean isFull() {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (cells[i][j] == ' ') {
+                            return false;
+                        }
+                    }
+                }
         return true;
     }
 
